b10779ffe928d1c6b8b8bd7da531f493a41f67c4
Mahder Belete Bekele <mahderbeletebekele@gmail.com>
2025-06-20 18:18:02 +0300

    Added app users

---
+++ sokoconnect/users/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Customer
+admin.site.register(Customer)
+
+from .models import MamaMboga
+admin.site.register(MamaMboga)
+# Register your models here.
