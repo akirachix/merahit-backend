3fcf85c3edfc2ec58f9d3b657aac3b051b752fe3
Mahder Belete Bekele <mahderbeletebekele@gmail.com>
2025-06-20 18:18:02 +0300

    Added app users

--- sokoconnect/sokoconnect/settings.py
+++ sokoconnect/sokoconnect/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -73,11 +74,17 @@ WSGI_APPLICATION = 'sokoconnect.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'HOST':'aws-0-eu-north-1.pooler.supabase.com',
+        'PORT' : '5432',
+        'USER':'postgres.vhiiixndtxeyaqzekmks',
+        'PASSWORD' : '1321Nm!!!'
     }
+
 }
+
 
 
 # Password validation
@@ -104,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
