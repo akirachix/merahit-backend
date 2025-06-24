990da15d365892ef3625f7164ffda34370ab68b6
Mahder Belete Bekele <mahderbeletebekele@gmail.com>
2025-06-24 21:57:07 +0300

    Added an end point for customers and Mamambogas

---
+++ sokoconnect/api/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from users.models import MamaMboga, Customer
+from .serializers import MamaMbogaSerializer, CustomerSerializer
+# from users.models import Customer
+# from .serializers import CustomerSerializer
+
+
+
+class MamaMbogaViewSet(viewsets.ModelViewSet):
+    queryset=MamaMboga.objects.all()
+    serializer_class=MamaMbogaSerializer
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset=Customer.objects.all()
+    serializer_class=CustomerSerializer
+# Create your views here.
