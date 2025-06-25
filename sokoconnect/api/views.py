49d1b3546f2ea32bcc8224cdfcc3d5bf00acfc8e
birhanu <birhanutbelay@gmail.com>
2025-06-25 06:45:33 +0300

    Created API end points

--- sokoconnect/api/views.py
+++ sokoconnect/api/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 from rest_framework import viewsets
 from users.models import MamaMboga, Customer
-from .serializers import MamaMbogaSerializer, CustomerSerializer
+from order.models import Order,OrderItem,Payment,Cart
+from .serializers import MamaMbogaSerializer, CustomerSerializer,OrderSerializer,OrderItemSerializer,PaymentSerializer,CartSerializer
 # from users.models import Customer
 # from .serializers import CustomerSerializer
 
@@ -15,4 +16,20 @@ class MamaMbogaViewSet(viewsets.ModelViewSet):
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset=Customer.objects.all()
     serializer_class=CustomerSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset=Order.objects.all()
+    serializer_class=OrderSerializer
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset=OrderItem.objects.all()
+    serializer_class=OrderItemSerializer
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset=Payment.objects.all()
+    serializer_class=PaymentSerializer
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset=Cart.objects.all()
+    serializer_class=CartSerializer
 # Create your views here.
